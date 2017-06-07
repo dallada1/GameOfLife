bbf1b4fecfe7a0b267053af5adb440262f3635ad
qualtrax\dallada <dallada@qualtrax.com>
2017-06-07 09:59:21 -0400

    All tests done

--- GameOfLife.Tests/Controllers/HomeControllerTest.cs
+++ GameOfLife.Tests/Controllers/HomeControllerTest.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GameOfLife;
 using GameOfLife.Controllers;
 using GameOfLife.Models;
+using System.Collections.Generic;
+using GameOfLifeDomain;
 
 namespace GameOfLife.Tests.Controllers
 {
@@ -70,5 +69,84 @@ namespace GameOfLife.Tests.Controllers
             Assert.IsTrue(modelAfterIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 2).IsAlive);
             Assert.IsTrue(modelAfterIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 2 && c.YCoordinate == 2).IsAlive);
         }
+
+        [TestMethod]
+        public void SeedGridFromTemplateWithBoxAtOneOne()
+        {
+            var result = controller.SeedGridFromTemplate("1,1", "Box") as ViewResult;
+            var modelBeforeIncrementGeneration = result.Model as GameOfLifeModel;
+
+            var modelAfterIncrementGeneration = result.Model as GameOfLifeModel;
+
+            Assert.IsTrue(modelAfterIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 1).IsAlive);
+            Assert.IsTrue(modelAfterIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 2).IsAlive);
+            Assert.IsTrue(modelAfterIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 2 && c.YCoordinate == 1).IsAlive);
+            Assert.IsTrue(modelAfterIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 2 && c.YCoordinate == 2).IsAlive);
+        }
+
+        [TestMethod]
+        public void SeedGridFromTemplateWithVerticalLineAtOneOne()
+        {
+            var result = controller.SeedGridFromTemplate("1,1", "VerticalLine") as ViewResult;
+            var modelBeforeIncrementGeneration = result.Model as GameOfLifeModel;
+
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 1).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 2 && c.YCoordinate == 1).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 3 && c.YCoordinate == 1).IsAlive);
+        }
+
+        [TestMethod]
+        public void SeedGridFromTemplateWithHorizontalLineAtOneOne()
+        {
+            var result = controller.SeedGridFromTemplate("1,1", "HorizontalLine") as ViewResult;
+            var modelBeforeIncrementGeneration = result.Model as GameOfLifeModel;
+
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 1).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 2).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 3).IsAlive);
+        }
+
+        [TestMethod]
+        public void SeedGridFromTemplateWithToadOneOne()
+        {
+            var result = controller.SeedGridFromTemplate("1,1", "Toad") as ViewResult;
+            var modelBeforeIncrementGeneration = result.Model as GameOfLifeModel;
+
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 1).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 2).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 3).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 2 && c.YCoordinate == 2).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 2 && c.YCoordinate == 3).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 2 && c.YCoordinate == 4).IsAlive);
+        }
+
+        [TestMethod]
+        public void SeedGridFromTemplateWithGliderOneOne()
+        {
+            var result = controller.SeedGridFromTemplate("1,1", "Glider") as ViewResult;
+            var modelBeforeIncrementGeneration = result.Model as GameOfLifeModel;
+
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 1).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 1 && c.YCoordinate == 3).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 2 && c.YCoordinate == 2).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 3 && c.YCoordinate == 2).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 2 && c.YCoordinate == 3).IsAlive);
+            Assert.IsTrue(modelBeforeIncrementGeneration.CellsModel.Cells.First(c => c.XCoordinate == 3 && c.YCoordinate == 2).IsAlive);
+        }
+
+        [TestMethod]
+        public void PressPlayThenPauseButton()
+        {
+            var result = controller.Play(new List<Cell> {new Cell(1,1)}) as ViewResult;
+            var modelBeforeIncrementGeneration = result.Model as GameOfLifeModel;
+
+            Assert.IsTrue(modelBeforeIncrementGeneration.AutoRun);
+
+            result = controller.Pause(new List<Cell> { new Cell(1, 1) }) as ViewResult;
+            modelBeforeIncrementGeneration = result.Model as GameOfLifeModel;
+
+            Assert.IsFalse(modelBeforeIncrementGeneration.AutoRun);
+        }
     }
+
 }
--- GameOfLife/Controllers/HomeController.cs
+++ GameOfLife/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using GameOfLifeDomain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GameOfLife.Controllers
